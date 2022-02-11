23ee23fec05c67a23693259b017c9beca7952466
DDeenis <ddeenis46@gmail.com>
2022-02-11 09:20:37 +0200

    sticky header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -6,7 +6,7 @@ import { UserBar } from "./UserBar";
 export const Header = () => {
   const user = useUser();
   return (
-    <header className="w-full border-b-[1px] border-gray-300 bg-white md:px-3 xl:px-0">
+    <header className="w-full border-b-[1px] border-gray-300 bg-white md:px-3 xl:px-0 block lg:sticky left-0 right-0 top-0 z-10">
       <div className="w-full mx-auto flex justify-between flex-wrap p-3 md:p-2 lg:p-0 max-w-[1096px]">
         <MainNav />
         <UserBar user={user} />
