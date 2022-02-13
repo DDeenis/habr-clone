f9e78e1a245acda0f0dcc0bdd0deea97b74d3a4c
DDeenis <ddeenis46@gmail.com>
2022-02-13 15:34:15 +0200

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,20 @@ module.exports = {
     es2021: true,
   },
   extends: ["plugin:react/recommended"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: "latest",
     sourceType: "module",
+    requireConfigFile: false,
   },
   plugins: ["react"],
   rules: {
     "react/prop-types": "off",
     "react/display-name": "off",
+    "react/react-in-jsx-scope": "off",
   },
+  ignorePatterns: ["node_modules/"],
 };
