2d80ac66d8f66971b08733802b8b68892e49724d
DDeenis <ddeenis46@gmail.com>
2022-03-20 13:12:54 +0200

    format date correctly

--- src/components/Article/components/ArticleUserInfo.tsx
+++ src/components/Article/components/ArticleUserInfo.tsx
@@ -1,6 +1,7 @@
 import { Avatar } from "@/components/common/Avatar";
+import { formatArticleDate } from "@/helpers/stringUtils";
 import { User } from "@/types/user";
-import { formatRelative } from "date-fns";
+import { format, formatRelative } from "date-fns";
 import { ru } from "date-fns/locale";
 import React from "react";
 
@@ -10,9 +11,13 @@ type Props = {
 };
 
 export const ArticleUserInfo = ({ user, publishedAt }: Props) => {
-  const dateFormatted = formatRelative(publishedAt, Date.now(), {
-    locale: ru,
-  });
+  // const dateFormatted = formatRelative(publishedAt, Date.now(), {
+  //   locale: ru,
+  // });
+  // const dateFormatted = format(publishedAt, "d MMMM в hh:mm", {
+  //   locale: ru,
+  // });
+  const dateFormatted = formatArticleDate(publishedAt);
 
   return (
     <div className="flex gap-2 items-center justify-start">
@@ -21,11 +26,13 @@ export const ArticleUserInfo = ({ user, publishedAt }: Props) => {
         className="flex gap-2 items-center justify-start"
       >
         <Avatar size="sm" image={user.avatar} />
-        <span className="text-sm font-bold hover:text-blue-800 transition-colors">
+        <span className="text-[.8125rem] font-bold hover:text-blue-800 transition-colors">
           {user.username}
         </span>
       </a>
-      <span className="text-sm text-gray-400 font-medium">{dateFormatted}</span>
+      <span className="text-[.8125rem] text-gray-400 font-medium">
+        <time dateTime={publishedAt.toISOString()}>{dateFormatted}</time>
+      </span>
     </div>
   );
 };
--- src/helpers/stringUtils.ts
+++ src/helpers/stringUtils.ts
@@ -1,2 +1,29 @@
+import {
+  format,
+  formatRelative,
+  isWithinInterval,
+  startOfYesterday,
+} from "date-fns";
+import { ru } from "date-fns/locale";
+
 export const capitalize = (source: string) =>
   source[0].toUpperCase() + source.substring(1);
+
+export const formatArticleDate = (date: Date) => {
+  const yesterday = startOfYesterday();
+
+  if (
+    isWithinInterval(date, {
+      start: yesterday,
+      end: Date.now(),
+    })
+  ) {
+    return formatRelative(date, Date.now(), {
+      locale: ru,
+    });
+  }
+
+  return format(date, "d MMMM в hh:mm", {
+    locale: ru,
+  });
+};
